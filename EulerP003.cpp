3b66647901c29876d2a7a1e3aadf5b39472d44e6
pandic <cperez2122@gmail.com>
2016-05-27 17:39:05 +0200

    Create EulerP003.cpp

---
+++ EulerP003.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : EulerP003.cpp
+// Author      : Carlos Perez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : The prime factors of 13195 are 5, 7, 13 and 29.
+// 				 What is the largest prime factor of the number 600851475143 ?
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include    <cstdlib>
+#include    <ctime>
+#include    <sys/timeb.h>
+
+using namespace std;
+
+int main() {
+	const double NUMERO = 600851475143;
+	double Naux = NUMERO;
+	double i = 0.0;
+	cout.precision(18);
+	i = 0;
+	while (i <= Naux)
+	{
+		if ( fmod(NUMERO,i)== 0.0)
+		{
+			cout <<"Valor " << i <<endl;
+			Naux = Naux / i;
+		}
+		i = i + 1 ;
+	}
+	return 0;
+}
