7eeefdb53deae1b025fd14b8cfc0810193e18e99
pandic <cperez2122@gmail.com>
2016-05-27 17:39:21 +0200

    Update EulerP003.cpp

--- EulerP003.cpp
+++ EulerP003.cpp
@@ -4,7 +4,7 @@
 // Version     :
 // Copyright   : Your copyright notice
 // Description : The prime factors of 13195 are 5, 7, 13 and 29.
-// 				 What is the largest prime factor of the number 600851475143 ?
+// 		 What is the largest prime factor of the number 600851475143 ?
 //============================================================================
 
 #include <iostream>
