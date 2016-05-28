6b7ef10702f826e589889ccf6cf56b22b8104b1c
pandic <cperez2122@gmail.com>
2016-05-28 11:56:46 +0200

    Update EulerP007.cpp

--- EulerP007.cpp
+++ EulerP007.cpp
@@ -5,10 +5,10 @@
 // Copyright   : Your copyright notice
 // Description : Exercise number 07 from https://projecteuler.net/archives
 //
-//				 By listing the first six prime numbers:
-//				 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+//		 By listing the first six prime numbers:
+//		 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 //
-//				 What is the 10 001st prime number?
+//		 What is the 10 001st prime number?
 //============================================================================
 
 #include <iostream>
