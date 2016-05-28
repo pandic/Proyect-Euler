4a1745556082af74006807db71d63cda00298f80
pandic <cperez2122@gmail.com>
2016-05-28 13:04:32 +0200

    Update EulerP008.cpp

--- EulerP008.cpp
+++ EulerP008.cpp
@@ -3,7 +3,9 @@
 // Author      : Carlos Perez
 // Version     :
 // Copyright   : Your copyright notice
-// Description : The four adjacent digits in the 1000-digit number that have
+// Description : Exercise number 08 from https://projecteuler.net/archives
+//
+//		The four adjacent digits in the 1000-digit number that have
 //		the greatest product are 9 × 9 × 8 × 9 = 5832.
 //
 //		73167176531330624919225119674426574742355349194934
